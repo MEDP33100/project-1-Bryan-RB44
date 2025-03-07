9b5329c415fd378f2e1b1488981084a3bbf8b674
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-07 02:08:44 -0500

    WIP 2

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //Start Page Functions
+
+//Function for Pluto Moving using GSAP
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.fromTo(".startPluto", 
+        { x: 0 },
+        { x: "100vw", duration: 3, repeat: -1, ease: "linear" }
+    );
+});
 //Function for Title Text
 
 //Function for Audio on Start Page
