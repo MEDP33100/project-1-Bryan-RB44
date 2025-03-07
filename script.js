0d697af992201335b224a276455887f0302a283d
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-07 02:22:05 -0500

    Start Page Design: Done (?)

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 //Function for Pluto Moving using GSAP
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.fromTo(".startPluto", 
-        { x: 0 },
-        { x: "100vw", duration: 3, repeat: -1, ease: "linear" }
+        { x: -150 },
+        { x: "100vw", duration: 60, repeat: -1, ease: "linear" }
     );
 });
 //Function for Title Text
