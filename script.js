002d6d176dad0cd92ea13f23985bdea08faf184a
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-08 01:23:25 -0500

    WIP 5
    Added the start screen to work successfully.
    Still needs:
    - Sound Effect on Button
    - Have the sections show up one at a time after clicking a button.
    - Implimenting said button.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     //Function for Title Text
     gsap.to("#titleStart", {
-        duration: 5,
+        duration: 4,
         repeat: -1,
         yoyo: true,
         scale: 1.1,
@@ -19,4 +19,31 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
 //Function for Audio on Start Page
 
-//Function for Start Button
+//Start Button
+//(Again using GSAP)
+document.addEventListener("DOMContentLoaded", () => {
+    const beginButton = document.querySelector(".startStory");
+    const startScreen = document.querySelector("header");
+    const sections = document.querySelectorAll("section");
+
+    beginButton.addEventListener("click", (e) => {
+        //Animate the header fading out and disappearing.
+        gsap.to(startScreen, {
+            duration: 1,
+            opacity: 0,
+            onComplete: () => {
+                startScreen.style.display = "none";
+            }
+        });
+
+        //Animation the story sections fading in.
+        sections.forEach(section => {
+            section.style.display = "block";
+            //Adding delay to start after the header fades out.
+            gsap.fromTo(section, 
+                { opacity: 0 }, 
+                { opacity: 1, duration: 1, delay: 1 }
+            );
+        });
+    });
+});
