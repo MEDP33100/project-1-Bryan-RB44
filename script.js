7bc052bf02aa5034f16aea2a225685362a2b2899
Bryan-RB44 <bryan.briones44@gmail.com>
2025-03-10 21:38:05 -0400

    WIP 11??
    N/A

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const beginButton = document.querySelector(".startStory");
     const startScreen = document.querySelector("header");
     const sections = document.querySelectorAll("section");
+    const sleepy = document.querySelector(".sleepy")
 
     beginButton.addEventListener("click", (e) => {
 
@@ -39,6 +40,15 @@ document.addEventListener("DOMContentLoaded", () => {
                 startScreen.style.display = "none";
             }
         });
+
+        //Animation for the sleepiness
+        gsap.to(sleepy, {
+            x: 50,
+            duration: 1,
+            repeat: -1,
+            yoyo: true,
+            ease: "power1.inOut"
+        })
 
         //Animation the story sections fading in.
         sections.forEach(section => {
